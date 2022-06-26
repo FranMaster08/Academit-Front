cea66382a27aa89fc005b98c52f2d0d0d5591800
fancisco Jimenez <devfran08@gmail.com>
2022-06-26 20:42:45 -0300

    add : se agrega sidebar mejorada ademas , se crea estilos para comportamiento responsive

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector(".menu-icon");
 const sidebar = document.querySelector(".sidebar");
-
+const container = document.querySelector(".container");
 menuIcon.addEventListener("click", () => {
   sidebar.classList.toggle("small-sidebar");
+  container.classList.toggle("large-container")
 });
