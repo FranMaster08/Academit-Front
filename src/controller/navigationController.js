92ec804e589c9e9a541308be016dbb41b9d7bf00
fancisco Jimenez <devfran08@gmail.com>
2022-07-07 00:05:29 -0300

    feature : se desacopla nav

--- src/controller/navigationController.js
+++ src/controller/navigationController.js
@@ -2,7 +2,12 @@
 const navigationController ={
     getHome : (req, res , next) => {
         res.render('index',{title: "Fran Generator"})
+    },
+    getVideo : (req, res , next) => {
+        res.render('play-videos',{title: "Fran Generator"})
     }
+
+
 }
 
 module.exports =navigationController
--- src/routes/home.js
+++ src/routes/home.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router();
 const { navigationController } = require('../controller')
 router.get('/',navigationController.getHome)
+router.get('/video',navigationController.getVideo)
 module.exports = router;
